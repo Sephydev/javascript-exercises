8ddc4f719c7eb7828815e48a0ee582b7a845afda
Sephydev <nicolas.bussiere@outlook.fr>
2024-12-19 12:21:55 +0100

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    splitStr = str.split('');
+    let reversedStr = splitStr.reverse().join('');
 
+    return reversedStr;
 };
 
 // Do not edit below this line
