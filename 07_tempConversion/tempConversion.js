9f05fd53a1672a53ac3f295b8455c11b2a9c1a1e
Sephydev <nicolas.bussiere@outlook.fr>
2024-12-19 19:46:10 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (far) {
+  let cel = ((far - 32) * (5 / 9));
+
+  if (cel % 1 !== 0) {
+    cel = Number(cel.toFixed(1));
+  }
+
+  return cel;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  let far = ((cel * (9 / 5) + 32));
+
+  if (far % 1 !== 0) {
+    far = Number(far.toFixed(1));
+  }
+
+  return far;
 };
 
 // Do not edit below this line
