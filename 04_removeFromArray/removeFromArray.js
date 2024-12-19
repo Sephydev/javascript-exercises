ee593245552b7e59f7a93e46c815aaaa0022ebc3
Sephydev <nicolas.bussiere@outlook.fr>
2024-12-19 19:09:52 +0100

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+    let filteredArray;
+
+    filteredArray = arr.filter(elem => !nums.includes(elem));
+
+    console.log(filteredArray);
+    return filteredArray;
 };
 
 // Do not edit below this line
