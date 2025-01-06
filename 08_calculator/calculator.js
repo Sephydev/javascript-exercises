6a5c1eecfd42fac68bd6dec67993889155003053
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-06 18:42:42 +0100

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = arr.reduce((total, current) => { return total + current; }, 0);
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = arr.reduce((total, current) => { return total * current }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
