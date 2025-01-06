2287cef10703e915623c55b0adf051f3b543f2d4
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-06 19:17:16 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    charArr = str.split('').filter(item => item !== '!' && item !== '.' && item !== ',' && item !== " " && typeof (item) !== "number");
+    copyCharArr = charArr.slice('');
+    copyCharArr.reverse();
+    let isPalindrome = true;
+    console.log(charArr);
+    charArr.forEach((item, index) => {
+        if (item.toLowerCase() !== copyCharArr[index].toLowerCase()) {
+            isPalindrome = false;
+        }
+    })
 
+    return isPalindrome;
 };
+
+palindromes("Animal loots foliated detail of stool lamina.")
 
 // Do not edit below this line
 module.exports = palindromes;
