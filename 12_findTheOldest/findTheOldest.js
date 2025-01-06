34c32dce6cdbc9a6971bec1b9063f56c72e2ec37
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-06 20:07:23 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const copyPeople = people.slice();
 
+    let oldestAge = 0;
+    let oldestPeople = {};
+    copyPeople.forEach((p) => {
+        if (!("yearOfDeath" in p)) {
+            p["yearOfDeath"] = new Date().getFullYear();
+        }
+
+        console.log(copyPeople);
+
+        if (p.yearOfDeath - p.yearOfBirth > oldestAge) {
+            oldestAge = p.yearOfDeath - p.yearOfBirth;
+            oldestPeople = p;
+        }
+    });
+    return oldestPeople;
 };
+
+findTheOldest([
+    {
+        name: "Carly",
+        yearOfBirth: 1066,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+])
 
 // Do not edit below this line
 module.exports = findTheOldest;
