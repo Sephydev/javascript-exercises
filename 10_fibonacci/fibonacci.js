592007b5e8143ace7c73b1a4076c6915f8352f30
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-06 19:31:36 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let fibo = [0, 1];
+    if (num >= 0) {
+        for (let i = 2; i <= num; i++) {
+            fibo.push(fibo[i - 2] + fibo[i - 1]);
+        }
+        return fibo[num];
+    } else {
+        return 'OOPS';
+    }
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
