fdc2a05f1b3bf6b10ba4083c15591be9af985b59
Sephydev <nicolas.bussiere@outlook.fr>
2025-12-30 10:41:14 +0100

    feat(06_repeatstring): Complete exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  
+  const stringArr = [];
 
+  for (let i = 0; i < num; i++) {
+    stringArr.push(string);
+  }
+
+  return stringArr.join("");
 };
 
 // Do not edit below this line
