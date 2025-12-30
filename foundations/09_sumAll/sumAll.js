db15da03f5a92ee5413ffaebc793a66db53cffd9
Sephydev <nicolas.bussiere@outlook.fr>
2025-12-30 11:11:06 +0100

    feat(09_sumAll): Complete exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (
+    typeof(num1) !== 'number'
+    || typeof(num2) !== 'number'
+    || num1 % 1 !== 0 
+    || num2 % 1 !== 0 
+    || num1 < 0 
+    || num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let arr = [];
+  let start = num1 > num2 ? num2 : num1;
+  let end = num1 > num2 ? num1 : num2;
+  
+  for (let i = start; i <= end; i++) {
+    arr.push(i)
+  }
+
+  return arr.reduce((acc, current) => acc + current);
 };
 
 // Do not edit below this line
