d2ae9a5f93c86a9277b142738af7e1eadfd5b68c
Sephydev <nicolas.bussiere@outlook.fr>
2026-01-06 12:19:44 +0100

    feat(fibonacci): Complete exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(numStr) {
+  const num = Number(numStr);
+  let firstNum = 0;
+  let secondNum = 1;
+
+  if (num === 0) {
+    return 0;
+  }
+
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 1; i < num; i++) {
+    let temp = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = temp; 
+  }
+
+  return secondNum;
 
 };
 
