74703451ff9c93991e2d9922a0e3ab2a84d5dd4a
Sephydev <nicolas.bussiere@outlook.fr>
2026-01-06 12:33:23 +0100

    feat(findTheOldest): Complete exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoples) {
 
+  const sortedPeoples = peoples.sort((a, b) => {
+    const aDeath = a.yearOfDeath || new Date().getFullYear();
+    const bDeath = b.yearOfDeath || new Date().getFullYear();
+
+    const firstPersonAge = aDeath - a.yearOfBirth;
+    const secondPersonAge = bDeath- b.yearOfBirth;
+    
+    return secondPersonAge - firstPersonAge;
+  })
+
+  return sortedPeoples[0];
 };
 
 // Do not edit below this line
