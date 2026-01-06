6750e86fcff6bf78d594e229febe8cd3a09518df
Sephydev <nicolas.bussiere@outlook.fr>
2026-01-06 11:54:14 +0100

    feat(palindromes): Complete exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const filteredStr = str.replace(/[!., ]/g, '').toLowerCase();
+  const strArray = filteredStr.split('');
+  const strArrayReversed = strArray.toReversed();
+  let isPalindrome = true;
 
+  
+  isPalindrome = strArray.every((cara, index) => {
+    let isSameCara = cara === strArrayReversed[index];
+
+    return isSameCara;
+  })
+
+  return isPalindrome;
 };
 
 // Do not edit below this line
