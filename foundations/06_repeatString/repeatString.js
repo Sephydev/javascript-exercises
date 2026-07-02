286ce1a492ca40a96eb195d1ee5c87f294dbd669
Sephydev <nicolas.bussiere@outlook.fr>
2026-07-02 18:36:01 +0200

    Complete exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, numOfRepeat) {
+  if (numOfRepeat < 0) return "ERROR";
 
+  let finalStr = "";
+
+  for (let i = 0; i < numOfRepeat; i++) {
+    finalStr += str;
+  }
+
+  return finalStr;
 };
 
 // Do not edit below this line
