1db5b7b642299c4c64039a3ea7d78872c0b6049d
Sephydev <nicolas.bussiere@outlook.fr>
2026-07-14 22:42:18 +0200

    Complete exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldestPerson = people.reduce((oldest, current) => {
+    const currentYearOfDeath = current.yearOfDeath ? current.yearOfDeath : (new Date()).getFullYear();
+    const oldestYearOfDeath = oldest.yearOfDeath ? oldest.yearOfDeath : (new Date()).getFullYear();
 
+    const currentAge = currentYearOfDeath - current.yearOfBirth;
+    const oldestAge = oldestYearOfDeath - oldest.yearOfBirth;
+
+    return currentAge > oldestAge ? current : oldest;
+  })
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
