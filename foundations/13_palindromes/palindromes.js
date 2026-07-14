f33064d90c2d2e11d6ca61f06e9acd0719e48d0e
Sephydev <nicolas.bussiere@outlook.fr>
2026-07-14 22:06:42 +0200

    Complete exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const filteredStr = str
+    .toLowerCase()
+    .split('')
+    .filter(c => !(c === ',' || c === ' ' || c === '.' || c === '!'))
+    .join('');
 
+  const reverseStr = filteredStr
+    .split('')
+    .reverse()
+    .join('');
+
+    console.log(reverseStr);
+
+  return reverseStr === filteredStr;
 };
 
 // Do not edit below this line
