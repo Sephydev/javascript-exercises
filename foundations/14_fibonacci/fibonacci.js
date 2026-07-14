8fa244050d07675f2a1d73e5d8d1e02ed3cc1736
Sephydev <nicolas.bussiere@outlook.fr>
2026-07-14 22:29:41 +0200

    Complete exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (Number(num) === 0) return 0;
 
+  if (num < 0) return "OOPS";
+
+  let previousResult = 0;
+  let result = 1;
+
+  for(let i = 1; i < num; i++) {
+    let temp = previousResult + result;
+    previousResult = result;
+    result = temp;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
